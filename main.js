cdd273a673c6f7d9549de8bcc2034778f825c9a9
elasri21 <islame.edu@gmail.com>
2022-02-10 12:27:43 +0100

    fixing some issues

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-// still empty
+const selected = document.querySelector(".selected");
+const jobs = Array.from(document.querySelectorAll(".jobs span"));
+
+jobs.forEach(job => {
+    job.addEventListener("click", function(){
+        selected.style.display = "block";
+        let container = document.createElement("div");
+        container.setAttribute("class", "container");
+        selected.appendChild(container);
+        let clickedJob = job.cloneNode(true);
+        container.appendChild(clickedJob);
+        let closeBtn = document.createElement("span");
+        let eks = document.createTextNode("X");
+        closeBtn.appendChild(eks);
+        closeBtn.setAttribute("class", "close");
+        container.appendChild(closeBtn);
+        container.style.display = "inline-block";
+        closeBtn.addEventListener("click",function(){
+            this.parentNode.remove();
+            if(!document.querySelector(".container")){
+                selected.style.display = "none"
+            }
+        }) 
+         
+
+    })
+})
+
+
+
+
